2dc02b633c396b5a3c29fe5c49b90a44c79e81f7
Hansucho <hgonzales59@gmail.com>
2024-02-29 22:50:53 -0600

    Fixed: Se ha agregado la opcion de contador de intentos de utilizar la aplicación

--- src/components/2.Secuencia/secuencia.vue
+++ src/components/2.Secuencia/secuencia.vue
@@ -3,6 +3,8 @@
     <div>
       <div>
     <button v-if="!counting" @click="startCountdown">Empezar</button>
+    <p v-if="intentos > 0">Intentos restantes: {{ intentos }}</p>
+    <p v-else>No quedan intentos.</p>
     <div v-if="counting">
       <p>Te quedan: {{ countdown }} segundos</p>
     </div>
@@ -24,6 +26,8 @@
             {{ Resultado }}
         </span>
       </div>
+  
+    
     </div>
   </template>
   
@@ -35,6 +39,7 @@
         hiddenNumber: null,
         options: [],
         Resultado: '',
+        intentos:2,
         counting: false,
         countdown: 10,
         timer: null // Declarar timer como una propiedad de datos
@@ -45,27 +50,50 @@
     },
     methods: {
       startCountdown() {
-        this.generateSequence();
+       // Detener el temporizador existente si está en funcionamiento
+        if (this.timer) {
+          clearInterval(this.timer);
+          this.timer = null;
+        }
+        
+        //Aqui si los intentos son mayoires a 0 que empiece a generar una nueva secuencia y empiece el conteo
+        if(this.intentos>0)
+        {
+        console.log(this.intentos)
+        this.intentos--;
+        console.log(this.intentos)
+        this.countdown = 10;
+        this.Resultado = ''; //reiniciamos el valor de nuestro resultado cuando le damos click
+        this.generateSequence(); //genberamos una nueva secuencia
         this.counting = true;
+     
         this.timer = setInterval(() => {
-          if (this.countdown > 0) {
+          if ((this.countdown > 0) && (this.intentos>=0)) {
             this.countdown--;
+            console.log(this.intentos)
           } else {
             clearInterval(this.timer);
             this.counting = false;
             this.countdown = 10;
+            //this.intentos--;
+            this.Resultado = "¡Tiempo! El número oculto era:" + this.hiddenNumber;
           }
         }, 1000);
+        }
+    
       },
       generateSequence() {
+
         this.sequence = [];
         this.options = [];
+        
         for (let numeros = 1; numeros <= 9; numeros++) {
           this.sequence.push(numeros);
         }
-        this.sequence = this.sequence.sort(() => Math.random() - 0.5).slice(0, 7);
+        this.sequence = this.sequence.sort(() => Math.random() - 0.5).slice(0, 8);
         this.hiddenNumber = this.sequence[Math.floor(Math.random() * this.sequence.length)];
         this.generateOptions();
+        //this.intentos = 5; // Establecer intentos al inicio del juego
       },
       generateOptions() {
         this.options = this.sequence.filter(num => num !== this.hiddenNumber).slice(0, 2);
@@ -73,17 +101,21 @@
         this.shuffleArray(this.options);
       },
       checkOption(option) {
+        
+
         if (this.counting) {
           clearInterval(this.timer);
           this.counting = false;
         }
         if (option === this.hiddenNumber) {
-          console.log('¡Correcto! El número oculto era:', this.hiddenNumber);
+          //console.log('¡Correcto! El número oculto era:', this.hiddenNumber);
           this.Resultado = "¡Correcto! El número oculto era:" + this.hiddenNumber;
         } else {
-          console.log('Incorrecto. Inténtalo de nuevo.');
+          //this.intentos--;
+          //console.log('Incorrecto. Inténtalo de nuevo.');
           this.Resultado = "Incorrecto El número oculto era:" + this.hiddenNumber;
         }
+        //this.intentos--; // Decrementar intentos
       },
       shuffleArray(array) {
         for (let i = array.length - 1; i > 0; i--) {
